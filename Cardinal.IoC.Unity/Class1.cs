6a67bf4212b9a0e2b693f201c16016070cdfc158
Nat <nathanael.mann@cit-design.co.uk>
2014-07-11 11:56:51 +0100

    Added unity version

---
+++ Cardinal.IoC.Unity/Class1.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Practices.Unity;
+
+namespace Cardinal.IoC.Unity
+{
+    public abstract class UnityContainerAdapter : ContainerAdapter<IUnityContainer>
+    {
+        protected UnityContainerAdapter() : this(new UnityContainer())
+        {
+        }
+
+        protected UnityContainerAdapter(IUnityContainer container) : base(container)
+        {
+        }
+
+        public override T Resolve<T>(string name)
+        {
+            return Container.Resolve<T>(name);
+        }
+
+        public override T Resolve<T>()
+        {
+            return Container.Resolve<T>();
+        }
+    }
+}
