3f8d00f7be3ed90048a86948f7c551e16b2ea224
Simon <simon@simonproctor.com>
2014-07-24 15:09:31 +0100

    Fixes for unit tests and autofac specifics

--- Cardinal.IoC/ContainerAdapter.cs
+++ Cardinal.IoC/ContainerAdapter.cs
@@ -8,7 +8,7 @@ namespace Cardinal.IoC
     {
         protected ContainerAdapter()
         {
-            
+            Initialize();
         }
 
         protected ContainerAdapter(TContainer container)
